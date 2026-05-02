65a550e10a7a6ccc43445b51dfdb958478e799c0
Matthew Chudleigh <matthewtoday@gmail.com>
2026-05-02 16:06:46 +1000

    v0.6.2 - logging

--- src/postgres_mcp/__init__.py
+++ src/postgres_mcp/__init__.py
@@ -1,4 +1,6 @@
 import asyncio
+import logging
+import os
 import sys
 
 from . import server
@@ -7,6 +9,13 @@ from . import top_queries
 
 def main():
     """Main entry point for the package."""
+    # MCP stdio uses stdout for protocol; logs must go to stderr.
+    logging.basicConfig(
+        level=os.environ.get("POSTGRES_MCP_LOG_LEVEL", "INFO").upper(),
+        stream=sys.stderr,
+        format="%(asctime)s %(levelname)s %(name)s: %(message)s",
+    )
+
     # As of version 3.3.0 Psycopg on Windows is not compatible with the default
     # ProactorEventLoop.
     # See: https://www.psycopg.org/psycopg3/docs/advanced/async.html#async
